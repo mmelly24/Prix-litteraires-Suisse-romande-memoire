0ad7b8611c1abcfe8801895bda0b258e7f494ba3
mmelly24 <72981225+mmelly24@users.noreply.github.com>
2022-06-07 14:27:49 +0200

    Extraction du nom des lauréat-e-s des prix littéraires français
    Extraction du nom des lauréat-e-s des prix littéraires français Goncourt, Femina, Médicis, Renaudot et Interallié à l'aide d'un script python. Cette étape est nécessaire pour ensuite comparer le nom des auteur-e-s récompensés par ces prix avec ceux consacrés en Suisse romande.

---
+++ lauréats_prix_francais/interallie/prenom_interallie.py
@@ -0,0 +1,40 @@
+"""
+"""
+import re
+from pathlib import Path
+
+# Creation des chemins pour les fichiers source et destination
+DATA_FILE_PATH = Path("./laureats_interallie.txt")
+RESULTS_FILE_PATH = Path("./final_interallie.xslx")
+
+def main():
+    """Programme principal"""
+
+    # Recuperation du fichier source et stockage de son contenu dans une variable
+    if DATA_FILE_PATH.exists():
+        try:
+            with DATA_FILE_PATH.open(mode="r", encoding="utf-8") as fichier:
+                content = fichier.read()
+        except IOError:
+            print("Error while retrieving data from file.")
+    else:
+        print("Data file not found.")
+            
+    # Definition regex pour extraction des données
+    regex = re.compile(r"(\d+) \| ([A-Z].*) \| ([A-Z].*) \| (.*)")
+        
+    #Création d'une liste contenant les données correspondant à la regex
+    matchs = list()
+    for match in regex.finditer(content):
+        matchs.append(match)
+            
+    #Sauvergarde de la liste extraite dans le fichier destination
+    with RESULTS_FILE_PATH.open(mode="w", encoding="utf-8") as fichier:
+        for match in matchs:
+            fichier.write(f"{match.group(1)}\t{match.group(2)}\t{match.group(3)}\t{match.group(4)}\n")
+                
+
+
+if __name__ == "__main__":
+    main()
+
